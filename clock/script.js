c7afe5994ff041a8be6f1f4bbaf7017494f0b698
yikayiyo <837937787@qq.com>
2020-06-24 14:42:11 +0800

    Clock

---
+++ clock/script.js
@@ -0,0 +1,21 @@
+const secondHand = document.getElementById("second-hand");
+const minHand = document.getElementById("min-hand");
+const hourHand = document.getElementById("hour-hand");
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const mins = now.getMinutes();
+  const hours = now.getHours();
+
+  const secondsDegrees = (seconds / 60) * 360;
+  const minsDegrees = (mins / 60) * 360;
+  const hoursDegrees = (hours / 12) * 360;
+
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minHand.style.transform = `rotate(${minsDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
