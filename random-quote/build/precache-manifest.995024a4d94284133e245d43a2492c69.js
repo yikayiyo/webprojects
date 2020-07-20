1e69eb1acf950f7eb9d0f2b62c5793b2c5a4128e
yikayiyo <837937787@qq.com>
2020-07-20 19:29:41 +0800

    random quotes freecodecamp

---
+++ random-quote/build/precache-manifest.995024a4d94284133e245d43a2492c69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3528d23bb0e9fa8b58890c27cbf5e12b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4111a1a7379b9b768d95",
+    "url": "/static/css/main.5adc7217.chunk.css"
+  },
+  {
+    "revision": "a20257ac679c2a540df9",
+    "url": "/static/js/2.99449f35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.99449f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4111a1a7379b9b768d95",
+    "url": "/static/js/main.a8c0e66d.chunk.js"
+  },
+  {
+    "revision": "fae1ece45c3e6b09f21e",
+    "url": "/static/js/runtime-main.2aacfbfb.js"
+  }
+]);
