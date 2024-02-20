daa9f27dc4fd12b54b362a4495173c004f83d0f4
legao <837937787@qq.com>
2024-02-20 11:44:22 +0800

    tmp

--- animated-clip-path/main.js
+++ animated-clip-path/main.js
@@ -1,10 +1,5 @@
-gsap.registerPlugin(ScrollTrigger)
-const lenis = new Lenis()
-lenis.on('scroll', ScrollTrigger.update)
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000)
-})
-gsap.ticker.lagSmoothing(0)
+initAnimationPreset()
+preloadImages()
 
 // section-2 title
 gsap.to('.section-2__title', {
@@ -15,7 +10,7 @@ gsap.to('.section-2__title', {
     start: 'top center',
     end: '+=500',
     scrub: 1
-  },
+  }
 })
 
 // section-3
@@ -25,17 +20,18 @@ gsap
       trigger: '.section-3',
       start: 'top 250px', // 元素顶部 距离视图顶部50px
       end: 'top 50px', // 元素顶部 视图顶部
-      scrub: 1,
+      scrub: 1
     }
   })
   .to('.image-holder', {
-    rotation: 0,
+    rotate: 0,
     clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)'
   })
   .to(
     '.image-holder img',
     {
       scale: 2,
+      filter: 'blur(0px)',
       ease: 'power2.inOut'
     },
     0
@@ -66,7 +62,7 @@ paths.forEach((el) => {
     })
     .to(el, {
       ease: 'none',
-      attr: { d: pathTo },
+      attr: { d: pathTo }
     })
 })
 
@@ -79,7 +75,7 @@ images.forEach((el) => {
         trigger: el,
         start: 'top center',
         end: '+=150',
-        scrub: 1,
+        scrub: 1
       }
     })
     .fromTo(
@@ -90,16 +86,52 @@ images.forEach((el) => {
 })
 
 // footer
-gsap.timeline({
-  scrollTrigger: {
-    trigger: 'section:last-of-type',
-    start: 'top center',
-    end: '+=150',
-    scrub: 1,
-  }
-})
-.to(
-  'footer', {
-    opacity: 1,
-  }
-)
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: 'section:last-of-type',
+      start: 'top center',
+      end: '+=150',
+      scrub: 1
+    }
+  })
+  .to('footer', {
+    opacity: 1
+  })
+
+function preloadImages() {
+  const body = document.body
+  return new Promise((resolve) => {
+    const imgwrap = document.createElement('div')
+    imgwrap.style.visibility = 'hidden'
+    body.appendChild(imgwrap)
+
+    ;[...document.querySelectorAll('image')].forEach((el) => {
+      const imgEl = document.createElement('img')
+      imgEl.style.width = 0
+      imgEl.src = el.getAttribute('xlink:href')
+      imgEl.className = 'preload'
+      imgwrap.appendChild(imgEl)
+    })
+
+    const arrImg = [
+      ...document.querySelectorAll('.preload'),
+      ...document.querySelectorAll('.content__bg')
+    ]
+    imagesLoaded(arrImg, { background: true }, (_) => {
+      imgwrap.parentNode.removeChild(imgwrap)
+      body.classList.remove('loading')
+      resolve()
+    })
+  })
+}
+
+function initAnimationPreset() {
+  gsap.registerPlugin(ScrollTrigger)
+  const lenis = new Lenis()
+  lenis.on('scroll', ScrollTrigger.update)
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000)
+  })
+  gsap.ticker.lagSmoothing(0)
+}
