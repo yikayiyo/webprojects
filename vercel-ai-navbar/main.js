c8a2f5a3ca0555ec5815e7076a1fed59e2924b97
legao <837937787@qq.com>
2024-02-24 19:31:43 +0800

    tmp

--- vercel-ai-navbar/main.js
+++ vercel-ai-navbar/main.js
@@ -3,17 +3,41 @@ const navBtnsHighLight = document.querySelector(
   '.navigation-menu_tabsHighlight'
 )
 
+const panels = document.querySelectorAll('.navigation-menu_content')
+const navMenuViewportWrapper = document.querySelector('.navigation-menu_viewportPosition')
+const navMenuViewport = `
+  <div data-state="false" data-orientation="horizontal" class="navigation-menu_viewport">
+    ${Array.from(panels).reduce((a,b)=>a+b.outerHTML, '')}
+  </div>
+`
+
 navBtns.forEach((el) => {
+  const trigger = el.querySelector('.navigation-menu_trigger')
   el.addEventListener('mouseenter', () => {
-    console.log(el.getBoundingClientRect())
     const { x, width } = el.getBoundingClientRect()
     navBtnsHighLight.style = `
       opacity: 1; 
       transition-duration: 150ms; 
       width: ${width}px; 
-      transform: translate(${x - 153}px)`  // first item is 153px to the left
+      transform: translate(${x - 153}px)
+    `  // first item is 153px to the left
+    el.dataset.highlighted = 'true'
+    if(trigger) {
+      trigger.dataset.state = 'open'
+    }
+    const panel = el.querySelector('.navigation-menu_content')
+    console.log(panel);
+    if(panel) {
+      navMenuViewportWrapper.innerHTML = navMenuViewport
+    }
   })
+
   el.addEventListener('mouseleave', () => {
     navBtnsHighLight.style.opacity = 0
+    el.dataset.highlighted = 'false'
+    navMenuViewportWrapper.innerHTML = null
+    if(trigger) {
+      trigger.dataset.state = 'closed'
+    }
   })
 })
